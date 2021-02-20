b8856bf46ad6d4e1e7a60b0a855572aa5ffc5d3e
kazu-dot <takahashikazuhiro90@icoud.com>
2021-02-20 15:45:30 +0900

    スタート画面の編集、経過時間の表示

--- js/main.js
+++ js/main.js
@@ -1,40 +1,44 @@
 'use strict';
 
 {
-    function setWord() {
-        word = words.splice(Math.floor(Math.random() * words.length), 1)[0];
-        target.textContent = word;
-        loc = 0;
+  function setWord() {
+    word = words.splice(Math.floor(Math.random() * words.length), 1)[0];
+    target.textContent = word;
+    loc = 0;
+  }
+
+  const words = [
+    'red',
+    'blue',
+    'pink',
+  ];
+  let word;
+  let loc = 0;
+  let startTime;
+  const target = document.getElementById('target');
+
+  document.addEventListener('click', () => {
+    startTime = Date.now();
+    setWord();
+  });
+
+  document.addEventListener('keydown', e => {
+    if (e.key !== word[loc]) {
+      return;
     }
 
-    const words = [
-        'red',
-        'blue',
-        'black',
-        'white'
-    ];
-    let word;
-    let loc = 0;
-    const target = document.getElementById('target');
+    loc++;
+    target.textContent = '_'.repeat(loc) + word.substring(loc);
 
-    setWord();
+    if (loc === word.length) {
+      if (words.length === 0) {
+        const elapsedTime = ((Date.now() - startTime) / 1000).toFixed(2);
+        const result = document.getElementById('result');
+        result.textContent = `Finished! ${elapsedTime} seconds!`;
+        return;
+      }
 
-    document.addEventListener('keydown', e => {
-        if (e.key !== word[loc]) {
-            return;
-        }
-
-        loc++;
-        target.textContent = '_'.repeat(loc) + word.substring(loc);
-
-        if (loc === word.length) {
-            if (words.length === 0) {
-                const result = document.getElementById('result');
-                result.textContent = 'Finished!';
-                return;
-            }
-
-            setWord();
-        }
-    });
+      setWord();
+    }
+  });
 }
