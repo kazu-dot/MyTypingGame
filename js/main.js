da114b82ea9e1d3d2966d4288f340c9e22cc3b88
kazu-dot <takahashikazuhiro90@icoud.com>
2021-02-20 15:16:22 +0900

    重複する単語の削除

--- js/main.js
+++ js/main.js
@@ -1,38 +1,40 @@
-'user strict'
+'use strict';
 
 {
     function setWord() {
-        word = words[Math.floor(Math.random() * words.length)];
-        const target = document.getElementById('target');
+        word = words.splice(Math.floor(Math.random() * words.length), 1)[0];
         target.textContent = word;
         loc = 0;
     }
+
     const words = [
         'red',
-        'bleu',
-        'white',
-        'black'
+        'blue',
+        'black',
+        'white'
     ];
     let word;
-    let loc;
-
+    let loc = 0;
+    const target = document.getElementById('target');
 
     setWord();
 
     document.addEventListener('keydown', e => {
-        if (e.key != word[loc]) {
+        if (e.key !== word[loc]) {
             return;
         }
 
         loc++;
         target.textContent = '_'.repeat(loc) + word.substring(loc);
 
-
         if (loc === word.length) {
+            if (words.length === 0) {
+                const result = document.getElementById('result');
+                result.textContent = 'Finished!';
+                return;
+            }
+
             setWord();
         }
-
-
-
-    })
+    });
 }
